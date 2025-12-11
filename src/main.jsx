4a7c60d403d7b48d4e7837749c84dd2cdc70a657
Wojciech Potoniec <155390018+WojciechPotoniec@users.noreply.github.com>
2025-12-11 11:06:46 +0100

    minor fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
     // Il service worker viene generato automaticamente da vite-plugin-pwa
-    navigator.serviceWorker.register('/sw.js')
-      .then(registration => {
-        console.log('SW registrato:', registration)
-        
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("SW registrato:", registration);
+
         // Controlla aggiornamenti ogni ora
         setInterval(() => {
-          registration.update()
-        }, 60 * 60 * 1000)
+          registration.update();
+        }, 60 * 60 * 1000);
       })
-      .catch(error => {
-        console.log('SW registration fallita:', error)
-      })
-  })
+      .catch((error) => {
+        console.log("SW registration fallita:", error);
+      });
+  });
 }
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
