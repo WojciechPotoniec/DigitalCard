ebea435c33951d7ced8b06b8164e703afbed4fec
Wojciech Potoniec <155390018+WojciechPotoniec@users.noreply.github.com>
2025-12-11 11:28:03 +0100

    fixed logo view on mobile

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.nqrb3g91co8"
+    "revision": "0.c1sphprrjl8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
